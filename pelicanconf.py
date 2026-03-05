92ff38f3fa2c774a446c4d357be38a8f5c896a3f
David Cortez <hexod0t@protonmail.com>
2026-03-04 23:03:05 -0500

    set theme instead of blog for flisol cuenca website

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,33 +3,17 @@ SITENAME = 'Flisol Cuenca'
 SITEURL = ""
 
 PATH = "content"
+THEME = "theme"
 
 TIMEZONE = 'America/Guayaquil'
-
 DEFAULT_LANG = 'ES'
 
-# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+DIRECT_TEMPLATES = ['index']
 
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
-
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+DEFAULT_PAGINATION = False
