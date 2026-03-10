f34b4338bb3aa25ef32d1a5bd273fffaecc8e1c0
RamsisDev <dev.ramsisp@gmail.com>
2026-03-09 20:32:15 -0500

    Adicionado SEO para la web

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,5 @@
 AUTHOR = 'flisolcuenca'
-SITENAME = 'Flisol Cuenca'
+SITENAME = 'FLISOL Cuenca 2026'
 SITEURL = ""
 
 PATH = "content"
@@ -17,3 +17,43 @@ AUTHOR_FEED_RSS = None
 DIRECT_TEMPLATES = ['index']
 
 DEFAULT_PAGINATION = False
+
+
+# Variables para SEO 
+# ── Archivos estáticos en la raíz del output 
+STATIC_PATHS = ['extras']
+EXTRA_PATH_METADATA = {
+    'extras/robots.txt': {'path': 'robots.txt'},
+}
+
+# ── Templates que se renderizan directamente como archivos
+TEMPLATE_PAGES = {
+    'sitemap.xml': 'sitemap.xml',
+}
+
+SEO_DESCRIPTION = (
+    'Festival Latinoamericano de Instalación de Software Libre en Cuenca, Ecuador. '
+    'Charlas, talleres e InstallFest gratuitos el 16 de mayo de 2026 '
+    'en la Universidad Politecnica Salesiana de Cuenca.'
+)
+SEO_KEYWORDS = (
+    'FLISOL, software libre, Linux, Cuenca, Ecuador, instalación, '
+    'código abierto, open source, festival, tecnología, Ubuntu, Debian, '
+    'charlas, talleres, InstallFest'
+)
+OG_IMAGE = '/theme/img/og-flisol-cuenca-2026.jpg'
+
+# Datos del evento
+EVENT_NAME       = 'FLISOL Cuenca 2026'
+EVENT_DATE_START = '2026-05-16T09:00:00-05:00'
+EVENT_DATE_END   = '2026-05-16T17:00:00-05:00'
+EVENT_LOCATION   = 'Universidad de Cuenca, Av. 12 de Abril, Cuenca, Ecuador'
+EVENT_CITY       = 'Cuenca'
+EVENT_COUNTRY    = 'EC'
+EVENT_STATUS     = 'EventScheduled'
+EVENT_ATTENDANCE = 'OfflineEventAttendanceMode'
+
+# Organización 
+ORGANIZER_NAME   = 'FLISOL Cuenca'
+ORGANIZER_EMAIL  = 'contacto@flisolcuenca.org'
+TWITTER_HANDLE   = '@FLISOLCuenca'
